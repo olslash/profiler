11d536ccbc11404f9441247c2eddb352fd9ddfbd
Mitch R <olordhux+github@gmail.com>
2014-09-09 21:52:14 -0700

    testrunner file contains and exports the runner and chart drawing functions

--- testrunner.js
+++ testrunner.js
@@ -1,42 +1,11 @@
-var arrayGenerator = require('./arrayGenerator');
-
-
-google.load("visualization", "1", {packages:["corechart"]});
-google.setOnLoadCallback(drawChart);
-function drawChart() {
-  params = {
-    charset: {
-      // alpha: true, // random upper and lower letters
-      minNumber: 1,
-      maxNumber: 10
-    },
-    minLength: 5,
-    maxLength: 2000,
-    steps: 40
-  };
-  var options = {
-         title: 'Performance',
-         curveType: 'function'
-       };
-  var myGen = arrayGenerator(params);
-  var data = google.visualization.arrayToDataTable(runTest(doathing, myGen));
-  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-  chart.draw(data, options);
-}
-
-
-
-// given a function, run it until the generator returns undefn
-// time each function and insert results into rows of an array
-
 function runTest(fn, generator) {
   var results = [
     ['size', 'runtime']
   ];
-  
   while(true) {
+    var args = [].slice.call(arguments, 2);
     var dataset = generator();
-    
+    args.unshift(dataset);
 
     if(dataset === undefined) {
       return results;
@@ -44,7 +13,7 @@ function runTest(fn, generator) {
     var datasize = dataset.length;
     var start = window.performance.now();
     // var timer = process.hrtime();
-    fn(dataset);
+    fn.apply(null, args);
     // var executiontime = process.hrtime(timer);
     // var decimalseconds = (executiontime[1] / 1000000000).toFixed(2);
     // var executionresult = +executiontime[0] + +decimalseconds;
@@ -54,3 +23,23 @@ function runTest(fn, generator) {
 
   return results;
 }
+
+function drawChart(rawdata) {
+  var options = {
+         title: 'Performance',
+         curveType: 'function',
+         trendlines: { 0: {
+          color: "green",
+          lineWidth: 10
+         } }
+       };
+  var data = google.visualization.arrayToDataTable(rawdata);
+  
+  var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
+
+module.exports = {
+  runTest: runTest,
+  drawChart: drawChart
+};
